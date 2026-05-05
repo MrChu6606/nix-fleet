5a8e752283e9eeabfe158cbb8f6b61bcac1d19d7
Nicolas Cicchi <nmcicchi@gmail.com>
2026-05-05 15:47:41 -0400

    added kairo from github

--- modules/kairo.nix
+++ modules/kairo.nix
@@ -1,18 +1,27 @@
 { pkgs, ... }:
 
 let
-  kairo = pkgs.buildGoModule {
+
+  buildGoModule = pkgs.buildGoModule.override {
+    # Specify the go package needed for this project
+    go = pkgs.go_1_26;
+  };
+
+  kairo = buildGoModule {
     pname = "kairo";
     version = "unstable-2026-05-05";
+
 
     src = pkgs.fetchFromGitHub {
       owner = "programmersd21";
       repo = "kairo";
       rev = "main";
-      hash = "0rvdn144n4bgf5wmmpax7774wqdwp81z7qz4bwfs1fyni55f326n";
+      hash = "sha256-1ojhSonWu6AdX+Tj8wO6vGFOzjld3Vp5cW8RS0iwbWc=";
     };
 
-    vendorHash = null;
+    # Force Nix to ignore repos vendor folder
+    proxyVendor = true;
+    vendorHash = "sha256-yEgPXAWkSgLCPhvAfZ7PkT9G7rJzeIXJIWVfdQ+Di5g=";
   };
 in
 {
