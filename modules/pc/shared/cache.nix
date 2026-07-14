db73e59a7bd3a9299215503f841919ba40af51b1
MrChu6606 <nmcicchi@gmail.com>
2026-07-13 23:12:34 -0400

    added cache for rpi5 to shared pc modules

---
+++ modules/pc/shared/cache.nix
@@ -0,0 +1,12 @@
+_:{
+
+  nix.settings = {
+    # add cache for building rpi 5 kernel
+    substituters = [
+      "https://raspberry-pi-nix.cachix.org"
+    ];
+    trusted-public-keys = [
+      "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
+    ];
+  };
+}
