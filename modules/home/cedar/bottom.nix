fe5ef741eb81c364873633683a9e82d6468c8f89
MrChu6606 <nmcicchi@gmail.com>
2026-07-14 11:58:23 -0400

    added bottom for full system monitoring including gpu

---
+++ modules/home/cedar/bottom.nix
@@ -0,0 +1,17 @@
+_: {
+  programs.bottom = {
+    enable = true;
+    settings = {
+      # Core settings
+      flags = {
+        # Forces bottom to pull GPU data on startup
+        enable_gpu = true; 
+      };
+      
+      # Process widget column configuration
+      processes = {
+        columns = [ "PID" "Name" "CPU%" "Mem%" "GPU%" "GMem%" ];
+      };
+    };
+  };
+}
