db2597aeb7d09b10ff5c7d626f24473d689a3c35
MrChu6606 <nmcicchi@gmail.com>
2026-07-14 22:13:52 -0400

    added override to dashboards mnds settings because its on older nixpkgs version

--- modules/server/dashboard/networking.nix
+++ modules/server/dashboard/networking.nix
@@ -1,7 +1,11 @@
 { lib, ... }: {
   # Force-disable the settings attribute so the older nixpkgs doesn't panic
-  services.resolved.settings = lib.mkForce {};
-              
+  options.services.resolved.settings = lib.mkOption {
+    type = lib.types.anything;
+    default = {};
+    description = "Dummy option to prevent older nixpkgs from crashing on shared config";
+  };           
+
   services.resolved.extraConfig = ''
     MultiCastDNS=yes
   '';
