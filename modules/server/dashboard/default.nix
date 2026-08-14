60c4b8a0c553d22f2a100b30602c6738525f4946
MrChu6606 <nmcicchi@gmail.com>
2026-08-14 16:12:58 -0400

    pre move

--- modules/server/dashboard/default.nix
+++ modules/server/dashboard/default.nix
@@ -1,17 +1,4 @@
-{ loadModules, lib, pkgs, ... }:
+{ loadModules, ... }:
 {
   imports = loadModules ./. ++ [../pis/default.nix];
-
-  boot = {
-    loader.generic-extlinux-compatible.enable = lib.mkForce false;
-    initrd.availableKernelModules = pkgs.lib.mkForce [
-      # Standard RPi modules needed to boot
-      "vfat"
-      "ext4"
-      "sd_mod"
-      "bcm2835_dma"
-      "i2c_bcm2835"
-      "pcie_brcmstb" # Needed for RPi 4/5 PCIe/USB controllers
-    ];    
-  };
 }
