d379c6fdead5022b7e069a906ea03f17c0310da1
MrChu6606 <nmcicchi@gmail.com>
2026-08-30 13:21:13 -0400

    uhh merge?

--- modules/home/shared/vscode.nix
+++ modules/home/shared/vscode.nix
@@ -3,19 +3,21 @@
     enable = true;
     package = pkgs.vscode;
     
-    # Base extensions you want across ALL projects
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      mkhl.direnv
-    ];
+    profiles.default = {
+      # Base extensions you want across ALL projects
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        mkhl.direnv
+      ];
 
-    # Global Vim & Editor Preferences
-    userSettings = {
-      "vim.enable" = true;
-      "vim.useSystemClipboard" = true;
-      "vim.leader" = "<space>";
-      "workbench.colorTheme" = "Default Dark Modern";
-      "direnv.status.show" = "warning";
+      # Global Vim & Editor Preferences
+      userSettings = {
+        "vim.enable" = true;
+        "vim.useSystemClipboard" = true;
+        "vim.leader" = "<space>";
+        "workbench.colorTheme" = "Default Dark Modern";
+        "direnv.status.show" = "warning";
+      };
     };
   };
 }
